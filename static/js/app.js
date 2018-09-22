26b7536349db90493cb7cac399862414ac57914d
Geoff King <geoffrey_a_king@outlook.com>
2018-09-22 16:32:32 -0400

    HW14

---
+++ static/js/app.js
@@ -0,0 +1,253 @@
+// from data.js
+
+var tableData = data;
+
+
+
+// Loading the Table
+
+d3.select("tbody")
+
+  .selectAll("tr")
+
+  .data(tableData)
+
+  .enter()
+
+  .append("tr")
+
+  .html(function(d) {
+
+    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
+
+  });
+
+
+
+//Logic to search the data based on the date
+
+function dateSearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("datetime");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[0];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
+
+
+
+//Logic to search the data based on the CITY
+
+function citySearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("city");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[1];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
+
+
+
+//Logic to search the data based on the STATE
+
+function stateSearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("state");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[2];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
+
+
+
+//Logic to search the data based on the COUNTRY
+
+function countrySearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("country");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[3];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
+
+
+
+
+
+//Logic to search the data based on the SHAPE
+
+function shapeSearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("shape");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[4];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
+
+
+
+//Logic to search the data based on the DURATION
+
+function durationSearch() {
+
+  var input, filter, table, tr, td, i;
+
+  input = document.getElementById("duration");
+
+  filter = input.value.toUpperCase();
+
+  table = document.getElementById("ufo-table");
+
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[5];
+
+    if (td) {
+
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+
+        tr[i].style.display = "";
+
+      } else {
+
+        tr[i].style.display = "none";
+
+      }
+
+    }       
+
+  }
+
+}
